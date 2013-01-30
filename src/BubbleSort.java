a23523b54b5579976ac59d48a6d8a42476bda86f
Rylan Dirksen <rylandirksen@gmail.com>
2013-01-30 11:06:39 -0600

    Changed BubbleSort to be far more generic

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,8 +1,31 @@
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-abstract class BubbleSort {
+abstract class BubbleSort<TheTypeWeWant> {
 
-    abstract void main(ArrayList collectionToSort);
+    public final void sort(ArrayList collectionToSort){
+        Object first, second;
+        boolean swapHappened = true;
+
+        while(swapHappened){
+            swapHappened = false;
+
+            for(int x = 0; x < (collectionToSort.size() - 1); x++) {
+                first = collectionToSort.get(x);
+                second = collectionToSort.get(x+1);
+
+                if (compare(first, second)) {
+                    swap(x, collectionToSort, first, second);
+                    swapHappened = true;
+                }
+            }
+        }
+        printArray(collectionToSort);
+    }
+
+    abstract void swap(int index, ArrayList array, Object first, Object second);
+
+    abstract boolean compare(Object first, Object second);
 
     public final void printArray(ArrayList arrayToPrint){
         System.out.println("Outcome:");
@@ -10,4 +33,5 @@ abstract class BubbleSort {
             System.out.println(arrayToPrint.get(x));
         }
     }
+
 }
